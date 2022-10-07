45bafdca845d184e18cabbbd13366f1a871e9778
laelydp <laelydwiyanap@gmail.com>
2022-10-06 22:25:04 -0200

    Penambahan pengertian dan kegunaan string function

--- stringPHP.php
+++ stringPHP.php
@@ -8,14 +8,14 @@
     // akan menampilkan panjang variabel
     echo str_word_count($watashi);
     echo "<br/>";
-    //
+    //Mengitung jumlah kata dalam string
     echo strrev($watashi);
     echo "<br/>";
-    //
+    //Membalikkan string
     echo strpos($watashi, "Laely");
     echo "<br/>";
-    //
+    //strpos untuk mengakses suatu karakter atau kumpulan karakter tertentu pada string
     echo str_replace("Laely Dwiyana Putri", "Bayu", $watashi);
     echo "<br/>";
-    //
+    //Mengganti beberapa karakter dalam string
 ?>
